b981572efef2601a9a2a2442ae09d20014939f49
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-12 15:30:20 -0300

    feat: initial files

---
+++ src/main/java/br/com/italomiranda/DesafioBackEndTasks/DesafioBackEndTasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.italomiranda.DesafioBackEndTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/italomiranda/DesafioBackEndTasks/DesafioBackEndTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italomiranda.DesafioBackEndTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
