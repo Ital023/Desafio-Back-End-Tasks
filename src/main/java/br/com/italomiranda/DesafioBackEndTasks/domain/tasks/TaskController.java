883d9cdd56298457330e886c29ff9b9d43cf4b5f
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-12 17:33:00 -0300

    feat: Delete use case

--- src/main/java/br/com/italomiranda/DesafioBackEndTasks/domain/tasks/TaskController.java
+++ src/main/java/br/com/italomiranda/DesafioBackEndTasks/domain/tasks/TaskController.java
@@ -1,19 +1,28 @@
 package br.com.italomiranda.DesafioBackEndTasks.domain.tasks;
 
 import br.com.italomiranda.DesafioBackEndTasks.domain.tasks.UseCases.CreateTaskUseCase;
+import br.com.italomiranda.DesafioBackEndTasks.domain.tasks.UseCases.DeleteTaskUseCase;
 import br.com.italomiranda.DesafioBackEndTasks.domain.tasks.UseCases.ViewAllTasksUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private TasksRepository tasksRepository;
     @Autowired
     private CreateTaskUseCase create;
     @Autowired
     private ViewAllTasksUseCase view;
+    @Autowired
+    private DeleteTaskUseCase delete;
 
     @GetMapping
     public ResponseEntity allTasks(){
@@ -24,6 +33,12 @@ public class TaskController {
     public ResponseEntity createTask(@RequestBody Task task){
         Task taskSaved = create.createTask(task);
         return ResponseEntity.ok().body(taskSaved);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteTask(@PathVariable UUID id){
+        delete.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 
 
---
+++ src/main/java/br/com/italomiranda/DesafioBackEndTasks/domain/tasks/UseCases/DeleteTaskUseCase.java
@@ -0,0 +1,23 @@
+package br.com.italomiranda.DesafioBackEndTasks.domain.tasks.UseCases;
+
+import br.com.italomiranda.DesafioBackEndTasks.domain.tasks.Task;
+import br.com.italomiranda.DesafioBackEndTasks.domain.tasks.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class DeleteTaskUseCase {
+    @Autowired
+    private TasksRepository tasksRepository;
+
+    public void deleteTask(UUID id){
+        Optional<Task> task = tasksRepository.findById(id);
+
+        if(task.isPresent()){
+            tasksRepository.deleteById(id);
+        }
+    }
+}
